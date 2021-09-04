743dd8a91a637765e140b8208e38db501cab401e
huybn5776 <aseeme555076@gmail.com>
2021-09-04 18:03:40 +0800

    feat: fetch all news

--- src/views/NewsList/NewsList.vue
+++ src/views/NewsList/NewsList.vue
@@ -14,6 +14,7 @@ import NewsItemCard from '@views/NewsList/NewsItemCard/NewsItemCard.vue';
 import { useRoute, useRouter } from 'vue-router';
 
 import { getNews, topics } from '@api/google-news-api';
+import { NewsItem } from '@interfaces/news-item';
 import { NewsTopicItem } from '@interfaces/news-topic-item';
 
 const newsTopics = ref<NewsTopicItem[]>([]);
@@ -24,13 +25,34 @@ const router = useRouter();
 onMounted(async () => {
   const topic = route.params.topic as string;
   if (!topic) {
-    newsTopics.value = await getNews('topStories');
+    await fetchTopStories();
   } else if ((topics as string[]).includes(topic)) {
     newsTopics.value = await getNews(topic as typeof topics[number]);
   } else {
     await router.push({ name: 'news' });
   }
 });
+
+async function fetchTopStories(): Promise<void> {
+  newsTopics.value = await getNews('topStories');
+  newsTopics.value = [
+    ...newsTopics.value,
+    ...(await Promise.all([await getNews('worldAndNation'), await getNews('others')])).flatMap(
+      (newsTopicArray) => newsTopicArray,
+    ),
+  ];
+
+  const allNewsUrl = newsTopics.value.flatMap((topic) => [
+    ...topic.newsItems.map((news) => news.url),
+    ...topic.newsItems.flatMap((news) =>
+      ((news.relatedNewsItems || []) as NewsItem[]).map((relatedNews) => relatedNews.url),
+    ),
+  ]);
+
+  const headlineNewsTopic = (await getNews('headline'))[0];
+  headlineNewsTopic.newsItems = headlineNewsTopic.newsItems.filter((news) => !allNewsUrl.includes(news.url));
+  newsTopics.value = [...newsTopics.value, headlineNewsTopic];
+}
 </script>
 
 <style lang="scss" scoped>
