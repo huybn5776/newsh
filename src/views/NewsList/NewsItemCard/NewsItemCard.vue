7e50278cb3e09cbc3aa2c7ebfb1dcf5eb1ae74b6
huybn5776 <aseeme555076@gmail.com>
2021-09-05 16:04:50 +0800

    feat: news image

--- src/views/NewsList/NewsItemCard/NewsItemCard.vue
+++ src/views/NewsList/NewsItemCard/NewsItemCard.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="news-item-card">
-    <a :href="news.url" class="news-link" target="_blank">
-      <h3 class="news-title">{{ news.title }}</h3>
-      <NewsInfoBar :news="news" />
-    </a>
-
-    <div v-if="news.relatedNewsItems?.length" class="related-news-list">
-      <a
-        v-for="relatedNews of news.relatedNewsItems"
-        :key="relatedNews.url"
-        class="related-news-link"
-        target="_blank"
-        :href="relatedNews.url"
-      >
-        <div class="related-news-container">
-          <h4 class="related-news-title">{{ relatedNews.title }}</h4>
-          <NewsInfoBar :news="relatedNews" />
-        </div>
+    <div class="news-item">
+      <a :href="news.url" class="news-link" target="_blank">
+        <h3 class="news-title">{{ news.title }}</h3>
+        <NewsInfoBar :news="news" />
       </a>
+
+      <div v-if="news.relatedNewsItems?.length" class="related-news-list">
+        <a
+            v-for="relatedNews of news.relatedNewsItems"
+            :key="relatedNews.url"
+            class="related-news-link"
+            target="_blank"
+            :href="relatedNews.url"
+        >
+          <div class="related-news-container">
+            <h4 class="related-news-title">{{ relatedNews.title }}</h4>
+            <NewsInfoBar :news="relatedNews" />
+          </div>
+        </a>
+      </div>
     </div>
+
+    <img class="news-image" :src="news.image" :alt="news.excerpt" />
   </div>
 </template>
 
