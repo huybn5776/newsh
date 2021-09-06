121a1b15171fd280dca00b18e89f250de8873ccb
huybn5776 <aseeme555076@gmail.com>
2021-09-06 21:40:26 +0800

    fix: prevent unnecessary watchEffect callback when component first load

--- src/compositions/use-sync-setting.ts
+++ src/compositions/use-sync-setting.ts
@@ -1,17 +1,17 @@
-import { Ref, ref, watchEffect, UnwrapRef } from 'vue';
+import { Ref, ref, watch, UnwrapRef } from 'vue';
 
 import { SettingKey } from '@enums/setting-key';
 import { getSettingFromStorage, saveSettingToStorage } from '@utils/storage-utils';
 
 export function useSyncSetting<T>(key: SettingKey): Ref<UnwrapRef<T | null>> {
   const settingRef = ref(getSettingFromStorage<T>(key));
-  watchEffect(() => saveSettingToStorage(key, settingRef.value));
+  watch(settingRef, () => saveSettingToStorage(key, settingRef.value));
   return settingRef;
 }
 
 export function useSyncSettingMapUndefined<T>(key: SettingKey): Ref<UnwrapRef<T | undefined>> {
   const settingValue = getSettingFromStorage<T>(key);
   const settingRef = ref<T | undefined>(settingValue === null ? undefined : settingValue);
-  watchEffect(() => saveSettingToStorage(key, settingRef.value === undefined ? null : settingRef.value));
+  watch(settingRef, () => saveSettingToStorage(key, settingRef.value === undefined ? null : settingRef.value));
   return settingRef;
 }
