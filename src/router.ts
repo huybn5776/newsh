53a2534fb967dc14880a14f9590aa84b09589f30
huybn5776 <aseeme555076@gmail.com>
2021-09-06 15:54:01 +0800

    feat: settings page

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { createWebHistory, createRouter, RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   { path: '/news/:topic?', component: () => import('@views/NewsList/NewsList.vue'), name: 'news' },
+  { path: '/settings', component: () => import('@views/SettingsPage/SettingsPage.vue'), name: 'settings' },
   { path: '/:catchAll(.*)', redirect: { name: 'news' } },
 ];
 
---
+++ src/views/SettingsPage/SettingsPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="page-content">
+    <h1 class="setting-page-title">Settings</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+@import './SettingsPage.scss';
+</style>
