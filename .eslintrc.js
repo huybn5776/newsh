50afcbc11f45949a1d680f45243263047ba5e389
huybn5776 <aseeme555076@gmail.com>
2021-12-30 22:40:23 +0800

    chore: update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
     'eslint:recommended',
     '@vue/typescript/recommended',
     '@vue/prettier',
-    '@vue/prettier/@typescript-eslint',
+    '@vue/eslint-config-typescript'
   ],
   parser: 'vue-eslint-parser',
   parserOptions: {
